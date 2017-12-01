96367d92b6199effce1ac81aeb22094b1cfbf042
dirty <darby.chang@gmail.com>
2017-12-01 22:45:20 +0800

    refine classification

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ML-tutorial
+
+pip install --upgrade pip
+python3 -m venv ~/.virtualenvs/ml
+pip install jupyter
+pip install numpy
+pip install pydotplus
+pip install scipy
+pip install sklearn
+
+brew install graphviz
--- classification/.prepared/classification.py
+++ classification/.prepared/classification.py
@@ -1,9 +1,10 @@
-import numpy
 from IPython.display import Image 
+import numpy
+from os import path
 from pydotplus import graph_from_dot_data
-import os
 import random
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 from sklearn.tree import export_graphviz
 
 def classifier(X, y):
@@ -15,23 +16,25 @@ def evaluate(clf, X_train, X_test, y_train, y_test):
     print('Accuracy on training samples: %s' % (clf.score(X_train, y_train)))
     print('Accuracy on test samples: %s\n' % (clf.score(X_test, y_test)))
 
-def get_data(path):
-    pwd = os.path.dirname(__file__)
-    f = open(os.path.join(pwd, path), 'r')
+def german_credit_data():
+    pwd = path.dirname(__file__)
+    f = open(path.join(pwd, 'german_credit.csv'), 'r')
     feature_names = f.readline().split(',')[1:]
     data = numpy.loadtxt(f, delimiter=',', dtype=int)
 
-    random.seed(0)
-    random.shuffle(data)
-
-    return data[:, 1:], data[:, 0], feature_names
+    return train_test_split(
+            data[:, 1:],
+            data[:, 0],
+            random_state=0,
+            test_size=0.2
+            ) + [feature_names]
 
 def plot(clf):
     dot_data = export_graphviz(clf, class_names=['not worthy', 'worthy'], feature_names=feature_names, filled=True, out_file=None) 
     graph = graph_from_dot_data(dot_data) 
     display(Image(graph.create_png()))
 
-X, y, feature_names = get_data('german_credit.csv')
+X_train, X_test, y_train, y_test, feature_names = german_credit_data()
 
 if '__main__' == __name__:
     pass
