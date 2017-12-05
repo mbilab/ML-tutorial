9cd94748a46746d12b1c4a9517f2ce3fa74d62e0
p3w52016 <p3w52016@gmail.com>
2017-12-05 19:23:43 +0800

    add next_batch and change var names

--- gan/.prepared/gan.py
+++ gan/.prepared/gan.py
@@ -16,6 +16,7 @@ matplotlib.use('Agg') #! necessary?
 
 from tensorflow.examples.tutorials.mnist import input_data
 
+from sklearn.utils import shuffle
 from keras.layers import Dense, Activation, Flatten, Reshape
 from keras.layers import Conv2D, Conv2DTranspose, UpSampling2D
 from keras.layers import LeakyReLU, Dropout
@@ -23,16 +24,39 @@ from keras.layers import BatchNormalization
 from keras.models import load_model, Sequential
 from keras.optimizers import Adam, RMSprop
 
+def demo(noise_size):
+    model = load_model('./model.h5')
+    dcgan = DCGAN(28, 28, 1, generator=model)
+    pred = dcgan.G.predict(dcgan.noise(1, noise_size))
+    print(pred.shape)
+"""
+    for i in range(10):
+        file_name = folder + '/minist_%d.png' % i
+        noise = np.random.uniform(-1.0, 1.0, size=[16, 100])
+        images = model.predict(noise).reshape(-1, 28, 28, 1)
+        plt.figure(figsize=(10,10))
+        for i in range(images.shape[0]):
+            plt.subplot(4, 4, i+1)
+            image = images[i, :, :, :]
+            image = np.reshape(image, [28, 28])
+            plt.imshow(image, cmap='gray')
+            plt.axis('off')
+        plt.tight_layout()
+        plt.savefig(file_name)
+        plt.close('all')
+
+    print('Output Image Saved!')
+"""
 class DCGAN(object):
 
     def __init__(self, image_width, image_height, image_channels, generator=None, discriminator=None, noise_size=100):
         self.image_width = image_width
         self.image_height = image_height
         self.image_channels = image_channels
-        self.D = discriminator || self.default_discriminator()
-        self.G = generator || self.default_generator()
-        self.descriminator = discriminator_model() # (D)
-        self.adversarial = adversarial_model() # (G+D)
+        self.D = self.default_discriminator() if discriminator == None else discriminator
+        self.G = self.default_generator() if generator == None else generator
+        self.DM = self.discriminator_model() # (D)
+        self.AM = self.adversarial_model() # (G+D)
         self.noise_size = noise_size
 
     def adversarial_model(self):
@@ -71,28 +95,32 @@ class DCGAN(object):
         return model
 
     def fit(self, images, batch_size=256, epochs=10, plot_interval=0):
-        #noise_input = None
-        #if save_interval>0:
-        #    noise_input = np.random.uniform(-1.0, 1.0, size=[16, 100])
-        for i in range(train_steps):
-            batch = self.x_train[np.random.randint(0, self.images.shape[0], size=batch_size), :]
+        step = len(x_train)//batch_size
+        for i in range(epochs * step):
+            k = i % step
+
+            if k == 0:
+                self.x_train = shuffle(self.x_train)
+
+            batch = self.x_train[0*k:batch_size*k]
+            #batch = self.x_train[np.random.randint(0, self.images.shape[0], size=batch_size), :]
             noise = self.noise(batch_size)
-            fake_batch = self.generator.predict(noise)
+            fake_batch = self.G.predict(noise)
             x = np.concatenate((batch, fake_batch))
-            y = np.concatenate(np.ones([batch_size, 1], np.zeros([batch_size, 0]))
-            self.discriminator.trainable = True
-            d_loss = self.discriminator.train_on_batch(x, y)
+            y = np.concatenate(np.ones([batch_size, 1], np.zeros([batch_size, 0])))
+            self.DM.trainable = True
+            d_loss = self.DM.train_on_batch(x, y)
 
             y = np.ones([batch_size, 1])
             noise = self.noise(batch_size)
-            self.discriminator.trainable = False
-            a_loss = self.adversarial.train_on_batch(noise, y)
+            self.DM.trainable = False
+            a_loss = self.AM.train_on_batch(noise, y)
             print("%d: D: loss: %f, acc: %f, A: loss: %f, acc: %f" % (i,
                 d_loss[0], d_loss[1],
                 a_loss[0], a_loss[1]
                 ))
 
-            if 0 == plot_interval || (i + 1) % plot_interval:
+            if 0 == plot_interval | (i + 1) % plot_interval:
                 continue
             #self.plot_images(output, save2file=True, samples=noise_input.shape[0], noise=noise_input, step=(i+1))
 
@@ -115,6 +143,9 @@ class DCGAN(object):
         #plt.close('all')
         plt.show()
 
+    def save_g_model(self):
+        self.G.save('path')
+
 def test():
     mnist = input_data.read_data_sets('./mnist/', one_hot=True)
 
@@ -125,28 +156,6 @@ def test():
     def check_images():
         dcgan.plot_image(check_noise)
 
-def demo():
-    model = load_model('./.prepared/model.h5')
-    folder = './good_output'
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-
-    for i in range(10):
-        file_name = folder + '/minist_%d.png' % i
-        noise = np.random.uniform(-1.0, 1.0, size=[16, 100])
-        images = model.predict(noise).reshape(-1, 28, 28, 1)
-        plt.figure(figsize=(10,10))
-        for i in range(images.shape[0]):
-            plt.subplot(4, 4, i+1)
-            image = images[i, :, :, :]
-            image = np.reshape(image, [28, 28])
-            plt.imshow(image, cmap='gray')
-            plt.axis('off')
-        plt.tight_layout()
-        plt.savefig(file_name)
-        plt.close('all')
-
-    print('Output Image Saved!')
 
 if __name__ == '__main__':
-    demo()
+    demo(100)
