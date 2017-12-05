0c96f3067d1ad5b7c1375cc9fa50c71eb577277d
dirty <darby.chang@gmail.com>
2017-12-05 21:41:09 +0800

    demo_pla()

--- pla/.prepared/pla.py
+++ pla/.prepared/pla.py
@@ -1,7 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot():
+def demo_pla():
+    data = np.loadtxt('./pla.dat', np.float32)
+    step = 0
+    w = np.zeros(data.shape[1])
+    while True:
+        err = 0
+        for d in data:
+            x = np.append(1, d[:4])
+            if np.dot(w, x) * d[-1] <= 0:
+                err += 1
+                step += 1
+                w += x * d[-1]
+        if 0 == err: break
+    print('#%d' % (step), w)
+
+def plot(): # {{{
     size = 4
     plt.xlim(-size, size)
     plt.ylim(-size, size)
@@ -22,25 +37,26 @@ def plot():
         else:
             plt.scatter(_x[0], _x[1], facecolors='b', marker='x')  
 
-    plt.title('$ \#%d: %.1f + %.1f x_1 + %.1f x_2 = 0$' % tuple(np.append(count, w)))
+    plt.title('$ \#%d: %.1f + %.1f x_1 + %.1f x_2 = 0$' % tuple(np.append(step, w)))
     plt.show()
+# }}}
 
 def reset():
-    global count, w
-    count = 0
-    w = np.array([0, 0, 0], np.float32)
+    global step, w
+    step = 0
+    w = np.zeros(3)
 
 def update():
-    global count, w
+    global step, w
     for _x, _y in zip(x, y):
-        _x = np.append(1, _x) * 1 
+        _x = np.append(1, _x)
         if np.dot(w, _x) * _y <= 0:
-            count += 1
-            w += _y * _x
+            step += 1
+            w += _x * _y
             break
     plot()
 
-count = None
+step = None
 w = None
 x = np.array([[1, 2], [2, 0], [0, 3], [2, 3], [1, 0], [-2, -2], [0, -1], [-1, 1], [-3, 1]], np.int32)   
 y = np.array([1, 1, 1, 1, -1, -1, -1, -1, -1], np.int32)
