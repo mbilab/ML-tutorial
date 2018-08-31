2f5e6b70e35dc0215da89e8dab6f7e12cb01325b
uno <st9007a@gmail.com>
2018-08-31 15:58:08 +0800

    add tutorial of data preprocessing

---
+++ sinopac-9-10-uno/create_csv.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import numpy as np
+from random import randint
+
+def apply_zero(array):
+
+    mask = np.random.randint(0, 49, size = array.shape) == 0
+    array[mask] = np.zeros(array.shape)[mask]
+
+if __name__ == '__main__':
+
+    num_sample = 10000
+    file_name = 'hw2.csv'
+    delimiter = '\t'
+    use_missing_value = True
+    num_label = 23
+
+    f1 = np.random.normal(170, 5, num_sample)
+    f2 = np.random.normal(20, 3, num_sample)
+    f3 = np.random.normal(50, 13, num_sample)
+    f4 = np.random.normal(80, 7, num_sample)
+    f5 = np.random.normal(60, 11, num_sample)
+
+    data = np.transpose(np.stack([f1, f2, f3, f4, f5]))
+
+    if use_missing_value:
+        apply_zero(data)
+
+    label = np.reshape(np.array([randint(0, num_label - 1) for _ in range(num_sample)]), [num_sample, 1])
+
+    data = np.concatenate([label, data], axis = 1)
+
+
+    with open(file_name, 'w') as f:
+
+        for sample in data:
+            sample = [str(int(sample[0]))] + ['%.1f' % num if num != 0 else 'none' for num in sample[1:].tolist()]
+
+            f.write(delimiter.join(sample) + '\n')
