2f2d2dced0aa9e382bb81ce5603c74b109107d1f
uno <st9007a@gmail.com>
2018-09-06 11:40:30 +0800

    Fix numpy version answer of data preprocessing

--- unit/data_preprocessing/.prepared/ex1_np.py
+++ unit/data_preprocessing/.prepared/ex1_np.py
@@ -4,4 +4,10 @@ import numpy as np
 
 data_matrix = np.loadtxt('../ex1.csv', delimiter = ',')
 
+label, other = np.split(data_matrix, [1], axis = 1)
+label = np.reshape(label, [-1]).astype(int)
+
+one_hot = np.eye(4)[label]
+
+data_matrix = np.concatenate([one_hot, other], axis = 1)
 print(data_matrix)
