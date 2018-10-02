3b0348131b6775ae06d61ed8a46e70bbe88952bf
dirty <darby.chang@gmail.com>
2018-10-02 22:00:10 +0800

    finalize regressor/

--- unit/regressor/.prepared/regressor.py
+++ unit/regressor/.prepared/regressor.py
@@ -24,23 +24,24 @@ def load_data():
     return train_X, train_Y, test_X, test_Y
 
 def demo():
-    # liner regression thr = 0.4, acc > 0.93
-    thr = 0.5
+    # liner regression th = 0.4, acc > 0.93
+    th = 0.5
     train_X, train_Y, test_X, test_Y = load_data()
-    clf = Pipeline([('scl', StandardScaler()),
-                    ('pca', PCA(n_components=2)),
-                    ('clf', SVR())])
+    clf = Pipeline([
+        ('scl', StandardScaler()),
+        ('pca', PCA(n_components=2)),
+        ('clf', SVR())
+    ])
     clf = clf.fit(train_X, train_Y)
     y_pred = clf.predict(test_X)
 
-    y_pred[y_pred >= thr] = 1
-    y_pred[y_pred < thr] = 0
+    y_pred[y_pred >= th] = 1
+    y_pred[y_pred < th] = 0
 
     return y_pred
 
 def evaluate(y_true, y_pred):
-    print("Test Accuracy: {:.3f}".format(accuracy_score(y_true, y_pred)))
+    print('Accuracy on test samples: %.3f' % (accuracy_score(y_true, y_pred)))
 
-
-if '__main__' == __name__:
-    demo()
+if __name__ is '__main__':
+    pass
