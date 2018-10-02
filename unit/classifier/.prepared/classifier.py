4b48be3924d5f895d8a22f6592f9aec17a3d5a84
dirty <darby.chang@gmail.com>
2018-10-02 20:04:27 +0800

    refining regressor/

--- unit/classifier/.prepared/classifier.py
+++ unit/classifier/.prepared/classifier.py
@@ -7,14 +7,22 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.tree import export_graphviz
 
-def demo(X, y):
+def demo(x, y):
     clf = RandomForestClassifier(n_estimators=500, random_state=0)
-    clf = clf.fit(X, y)
+    clf = clf.fit(x, y)
     return clf
 
-def evaluate(clf, X_train, X_test, y_train, y_test):
-    print('Accuracy on training samples: %s' % (clf.score(X_train, y_train)))
-    print('Accuracy on test samples: %s\n' % (clf.score(X_test, y_test)))
+def evaluate(clf, x_tr=None, x_te=None, y_tr=None, y_te=None):
+    if x_tr is None:
+        x_tr = x_train
+    if x_te is None:
+        x_te = x_test
+    if y_tr is None:
+        y_tr = y_train
+    if y_te is None:
+        y_te = y_test
+    print('Accuracy on training samples: %s' % (clf.score(x_train, y_train)))
+    print('Accuracy on test samples: %s\n' % (clf.score(x_test, y_test)))
 
 def german_credit_data():
     pwd = path.dirname(__file__)
@@ -23,11 +31,11 @@ def german_credit_data():
     data = numpy.loadtxt(f, delimiter=',', dtype=int)
 
     return train_test_split(
-            data[:, 1:],
-            data[:, 0],
-            random_state=0,
-            test_size=0.2
-            ) + [feature_names]
+        data[:, 1:],
+        data[:, 0],
+        random_state=0,
+        test_size=0.2
+    ) + [feature_names]
 
 def plot(clf):
     dot_data = export_graphviz(clf, class_names=['not worthy', 'worthy'], feature_names=feature_names, filled=True, out_file=None)
